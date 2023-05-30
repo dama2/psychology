16b0f7ee9308febe2768d98e26cbe4110d8f2cf5
dama2 <linama2@163.com>
2023-05-30 12:44:49 +0800

    test

--- components/Psychology/index.jsx
+++ components/Psychology/index.jsx
@@ -2,7 +2,7 @@
  * @Author: dama2 linama2@163.com
  * @Date: 2023-05-29 16:54:49
  * @LastEditors: dama2 linama2@163.com
- * @LastEditTime: 2023-05-29 22:23:53
+ * @LastEditTime: 2023-05-30 12:44:38
  * @FilePath: /psychology/components/Psychology/index.jsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,6 +11,7 @@ import { Route } from 'react-router'
 
 export default function Router() {
   return (
+    <div></div>
     
   )
 }
